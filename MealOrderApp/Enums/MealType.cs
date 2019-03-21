b9b7284b78d2d5c20f8f0186dfe45c5611f01def
Sachin Jambur <sachin.jambur@utcsdevtst.onmicrosoft.com>
2019-03-21 09:33:46 -0600

    Initial Commit

---
+++ MealOrderApp/Enums/MealType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MealOrderApp.Enums
+{
+    public enum MealType
+    {
+        Vegetarian,
+        NutFree,
+        FishFree,
+    }
+}
